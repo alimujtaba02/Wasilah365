7bbdc700c6aefec64c709e1eca050c9f1d82d179
alimujtaba02 <ali.mujtaba@dcmail.ca>
2025-12-13 23:29:15 -0500

    feat(home): working intention completion loop

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,13 +1,25 @@
-import { StyleSheet } from 'react-native';
-
 import { Text, View } from '@/components/Themed';
+import React, { useState } from 'react';
+import { Button, StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
+  const [isComplete, setIsComplete] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      <Text style={styles.title}>Today</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text>Welcome home.</Text>
+      {isComplete ? (
+        <Text>Intention marked as complete!</Text>
+      ) : (
+        <>
+          <Text>This is my intention today</Text>
+          <Button
+            title="Intention Complete"
+            onPress={() => setIsComplete(true)}
+          />
+        </>
+      )}
     </View>
   );
 }
